01f754801db5002e034c1c026b353941d5152ca5
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2025-11-05 22:44:33 +0800

    chore: add Dokka documentation generation [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
+import org.jetbrains.dokka.gradle.tasks.DokkaGenerateTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.net.URI
+
 
 plugins {
     java
     id("com.gradleup.shadow") version "9.0.0-beta15"
     id("org.jetbrains.kotlin.jvm") version "2.2.21"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.18"
+    id("org.jetbrains.dokka") version "2.1.0"
 }
 
 group = "io.github.heyhey123"
@@ -89,5 +94,42 @@ tasks {
         group = "build"
         description = "Build root project only"
         dependsOn(":build")
+    }
+    dokka {
+        dokkaPublications.html {
+            suppressInheritedMembers = true
+        }
+
+        dokkaSourceSets.main {
+            sourceRoots.from(file("src"))
+            documentedVisibilities.set(setOf(VisibilityModifier.Public))
+            jdkVersion = 21
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(URI("https://github.com/heyhey123-git/xiaojie-gui/tree/master/src/main/kotlin"))
+                remoteLineSuffix.set("#L")
+            }
+
+            externalDocumentationLinks {
+                register("skript") {
+                    url("https://docs.skriptlang.org/javadocs/")
+                    packageListUrl("https://docs.skriptlang.org/javadocs/element-list")
+                }
+                register("paper") {
+                    url("https://jd.papermc.io/paper/1.21.10/")
+                    packageListUrl("https://jd.papermc.io/paper/1.21.10/element-list")
+                }
+            }
+        }
+
+        pluginsConfiguration.html {
+//            customStyleSheets.from("styles.css")
+//            customAssets.from("logo.png")
+            footerMessage.set("Copyright (c) 2025 heyhey123, All rights reserved.")
+        }
+
+    }
+    withType<DokkaGenerateTask>().configureEach {
+        outputDirectory.set(layout.buildDirectory.dir("buildDocs/dokka"))
     }
 }
