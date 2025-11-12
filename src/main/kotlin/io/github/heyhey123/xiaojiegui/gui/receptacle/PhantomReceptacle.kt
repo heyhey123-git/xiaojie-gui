b59d04b8b7b8ad396cbad454560cca6b2e2cbc99
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2025-11-12 13:14:47 +0800

    Revert "optimize: cache converted itemstack"
    This reverts commit 28d63436

--- src/main/kotlin/io/github/heyhey123/xiaojiegui/gui/receptacle/PhantomReceptacle.kt
+++ src/main/kotlin/io/github/heyhey123/xiaojiegui/gui/receptacle/PhantomReceptacle.kt
@@ -4,13 +4,10 @@ import io.github.heyhey123.xiaojiegui.gui.PacketHelper
 import io.github.heyhey123.xiaojiegui.gui.event.ReceptacleInteractEvent
 import io.github.heyhey123.xiaojiegui.gui.interact.ClickType
 import io.github.heyhey123.xiaojiegui.gui.utils.TaskUtil
-import io.github.retrooper.packetevents.util.SpigotConversionUtil
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemStack
-
-private typealias PacketEventsItemStack = com.github.retrooper.packetevents.protocol.item.ItemStack
 
 class PhantomReceptacle(title: Component, layout: ViewLayout) : ViewReceptacle(title, layout, Mode.PHANTOM) {
 
@@ -18,14 +15,6 @@ class PhantomReceptacle(title: Component, layout: ViewLayout) : ViewReceptacle(t
      * The contents of the receptacle.
      */
     private val contents = arrayOfNulls<ItemStack?>(layout.totalSize)
-
-    private val convertedContent: Array<PacketEventsItemStack> =
-        Array(layout.totalSize) { PacketEventsItemStack.EMPTY }
-
-    /**
-     * A cache of current item in the player's off-hand.
-     */
-    private var currentOffHand: Pair<ItemStack, PacketEventsItemStack>? = null
 
     private var windowId: Int = -1
 
@@ -37,7 +26,6 @@ class PhantomReceptacle(title: Component, layout: ViewLayout) : ViewReceptacle(t
 
     override fun setElement(slot: Int, item: ItemStack?) {
         contents[slot] = item
-        convertedContent[slot] = SpigotConversionUtil.fromBukkitItemStack(item)
     }
 
     override fun doOpen(player: Player) {
@@ -72,24 +60,18 @@ class PhantomReceptacle(title: Component, layout: ViewLayout) : ViewReceptacle(t
         }
 
         if (event.clickType == ClickType.SWAP_OFFHAND) {
-            val offhand = viewer!!.equipment.itemInOffHand
-            if (currentOffHand == null || currentOffHand!!.first != offhand) {
-                currentOffHand = Pair(offhand, SpigotConversionUtil.fromBukkitItemStack(offhand))
-            }
-
-            val item = currentOffHand!!.second
             PacketHelper.instance.sendContainerSetSlot(
                 player = viewer!!,
                 windowId = 0,
                 slot = 45,
-                item
+                item = viewer!!.equipment.itemInOffHand
             )
         } else {
             PacketHelper.instance.sendContainerSetSlot(
                 player = viewer!!,
                 windowId = -1,
                 slot = -1,
-                item = PacketEventsItemStack.EMPTY
+                item = null
             )
         }
     }
@@ -98,10 +80,10 @@ class PhantomReceptacle(title: Component, layout: ViewLayout) : ViewReceptacle(t
         viewer ?: return
         setupPlayerInventory()
         if (slot >= 0) {
-            PacketHelper.instance.sendContainerSetSlot(viewer!!, windowId, slot, convertedContent[slot])
+            PacketHelper.instance.sendContainerSetSlot(viewer!!, windowId, slot, contents[slot])
             return
         }
-        PacketHelper.instance.sendContainerSetContent(viewer!!, windowId, convertedContent)
+        PacketHelper.instance.sendContainerSetContent(viewer!!, windowId, contents)
     }
 
     /**
@@ -119,7 +101,7 @@ class PhantomReceptacle(title: Component, layout: ViewLayout) : ViewReceptacle(t
             viewer!!,
             windowId = 0,
             slot = 45,
-            item = PacketEventsItemStack.EMPTY
+            item = null
         )
         refresh()
     }
@@ -138,8 +120,8 @@ class PhantomReceptacle(title: Component, layout: ViewLayout) : ViewReceptacle(t
                         in 9..35 -> layout.mainInvSlotRange[index - 9]
                         else -> -1
                     } // ensure slot is valid
-                if (slot >= 0 && contents[slot] != itemStack) {
-                    setElement(slot, itemStack)
+                if (slot >= 0) {
+                    contents[slot] = itemStack
                 }
             }
         }
--- src/test/kotlin/io/github/heyhey123/xiaojiegui/gui/receptacle/PhantomReceptacleTest.kt
+++ src/test/kotlin/io/github/heyhey123/xiaojiegui/gui/receptacle/PhantomReceptacleTest.kt
@@ -1,185 +1,166 @@
 package io.github.heyhey123.xiaojiegui.gui.receptacle
 
-// When mockk trying to load PacketEventsItemStack, the static initializer of PacketEvents will be triggered,
-// which causing NoClassDefFoundError, so we comment out the test for now.
+import io.github.heyhey123.xiaojiegui.gui.PacketHelper
+import io.github.heyhey123.xiaojiegui.gui.utils.TaskUtil
+import io.mockk.*
+import net.kyori.adventure.text.Component
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.PlayerInventory
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import java.util.*
+import kotlin.test.Test
 
-//import io.github.heyhey123.xiaojiegui.gui.PacketHelper
-//import io.github.heyhey123.xiaojiegui.gui.utils.TaskUtil
-//import io.mockk.*
-//import net.kyori.adventure.text.Component
-//import org.bukkit.entity.Player
-//import org.bukkit.inventory.ItemStack
-//import org.bukkit.inventory.PlayerInventory
-//import org.junit.jupiter.api.AfterEach
-//import org.junit.jupiter.api.BeforeEach
-//import java.util.*
-//import kotlin.test.Test
+class PhantomReceptacleTest {
 
-//
-//private typealias PacketEventsItemStack = com.github.retrooper.packetevents.protocol.item.ItemStack
-//
-//class PhantomReceptacleTest {
-//
-//    private lateinit var mockedPacketHelper: PacketHelper
-//
-//    private lateinit var mockedEmptyPacketEventsItemStack: PacketEventsItemStack
-//
-//    @BeforeEach
-//    fun setUp() {
-//        mockedPacketHelper = mockk<PacketHelper>()
-//        mockkObject(PacketHelper.Companion)
-//        every { PacketHelper.instance } returns mockedPacketHelper
-//        every { mockedPacketHelper.generateNextContainerId(any()) } returns 114
-//        mockedEmptyPacketEventsItemStack = mockk()
-//        every { mockedPacketHelper.emptyPacketEventsItemStack } returns mockedEmptyPacketEventsItemStack
-//    }
-//
-//    @AfterEach
-//    fun tearDown() {
-//        unmockkObject(PacketHelper.Companion)
-//    }
-//
-//    @Test
-//    fun `open receptacle and send init packets to player`() {
-//        val player = mockk<Player>(relaxed = true)
-//        every { player.uniqueId } returns UUID.randomUUID()
-//
-//        val title = Component.text("Phantom Receptacle Test")
-//        val layout = ViewLayout.Chest.GENERIC_9X3
-//
-//        every {
-//            mockedPacketHelper.sendOpenScreen(
-//                player, 114, layout.type, title
-//            )
-//        } just Runs
-//        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
-//        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, any()) } just Runs
-//
-//        every { mockedPacketHelper.convertToPacketEvents(any()) } returns mockk()
-//
-//        val receptacle = PhantomReceptacle(title, layout).apply {
-//            hidePlayerInventory = true // jump over player inventory setup
-//        }
-//        receptacle.open(player)
-//
-//        verify(exactly = 1) { mockedPacketHelper.generateNextContainerId(player) }
-//        verify(exactly = 1) {
-//            mockedPacketHelper.sendOpenScreen(
-//                player, 114, layout.type, title
-//            )
-//        }
-//        verify(exactly = 1) { mockedPacketHelper.sendContainerSetContent(player, 114, any()) }
-//        verify(exactly = 1) { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, any()) }
-//    }
-//
-//    @Test
-//    fun `set up player inventory slots and open`() {
-//        val player = mockk<Player>(relaxed = true)
-//        val inventory = mockk<PlayerInventory>(relaxed = true)
-//        val inventoryContents = arrayOfNulls<ItemStack?>(36)
-//
-//        val apple = mockk<ItemStack>()
-//        val bread = mockk<ItemStack>()
-//        inventoryContents[0] = apple // hotbar slot: 0, slot index: 30
-//        inventoryContents[17] = bread // main inv slot:17, slot index: 11
-//
-//        val title = Component.text("Phantom Receptacle Test")
-//        val layout = ViewLayout.FixedContainer.ANVIL
-//        val receptacle = PhantomReceptacle(title, layout).apply {
-//            hidePlayerInventory = false
-//        }
-//
-//        every { player.uniqueId } returns UUID.randomUUID()
-//        every { player.inventory } returns inventory
-//        every { inventory.contents } returns inventoryContents
-//
-//        val convertedApple = mockk<PacketEventsItemStack>()
-//        val convertedBread = mockk<PacketEventsItemStack>()
-//        every { mockedPacketHelper.convertToPacketEvents(apple) } returns convertedApple
-//        every { mockedPacketHelper.convertToPacketEvents(bread) } returns convertedBread
-//
-//        every { mockedPacketHelper.sendOpenScreen(player, 114, layout.type, any()) } just Runs
-//        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
-//        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, any()) } just Runs
-//
-//        receptacle.open(player)
-//
-//        // Verify that the player inventory slots are set up correctly
-//        verify {
-//            mockedPacketHelper.sendContainerSetContent(
-//                player,
-//                114,
-//                match<Array<PacketEventsItemStack>> { items -> items[30] == convertedApple && items[11] == convertedBread }
-//            )
-//        }
-//    }
-//
-//    @Test
-//    fun `retitle receptacle and send update to player`() {
-//        val player = mockk<Player>(relaxed = true)
-//        every { player.uniqueId } returns UUID.randomUUID()
-//
-//        val initialTitle = Component.text("Initial Title")
-//        val newTitle = Component.text("New Title")
-//        val layout = ViewLayout.Chest.GENERIC_9X3
-//
-//        every {
-//            mockedPacketHelper.sendOpenScreen(
-//                player, 114, layout.type, any()
-//            )
-//        } just Runs
-//        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
-//        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, any()) } just Runs
-//        every { mockedPacketHelper.convertToPacketEvents(any()) } returns mockk()
-//
-//        val receptacle = PhantomReceptacle(initialTitle, layout).apply {
-//            hidePlayerInventory = true // jump over player inventory setup
-//        }
-//        receptacle.open(player)
-//
-//        mockkObject(TaskUtil)
-//        every { TaskUtil.sync(period = 0L, now = false, delay = 3L, any()) } answers {
-//            receptacle.initializationPackets()
-//            mockk()
-//        }
-//
-//        // Retitle the receptacle
-//        receptacle.title(newTitle, render = true)
-//
-//        // Verify that the retitle packet was sent
-//        verify(exactly = 1) {
-//            mockedPacketHelper.sendOpenScreen(
-//                player, 114, layout.type, newTitle
-//            )
-//        }
-//
-//        unmockkObject(TaskUtil)
-//    }
-//
-//    @Test
-//    fun `send close packet on close with render true`() {
-//        val player = mockk<Player>(relaxed = true)
-//        every { player.uniqueId } returns UUID.randomUUID()
-//
-//        val title = Component.text("Phantom Receptacle Test")
-//        val layout = ViewLayout.Chest.GENERIC_9X3
-//
-//        every { mockedPacketHelper.sendOpenScreen(player, 114, layout.type, any()) } just Runs
-//        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
-//        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, any()) } just Runs
-//        every { mockedPacketHelper.sendContainerClose(player) } just Runs
-//        every { mockedPacketHelper.convertToPacketEvents(any()) } returns mockk()
-//
-//        val receptacle = PhantomReceptacle(title, layout).apply {
-//            hidePlayerInventory = true // jump over player inventory setup
-//        }
-//        receptacle.open(player)
-//
-//        // Close the receptacle with render = true
-//        receptacle.close(render = true)
-//
-//        // Verify that the close packet was sent
-//        verify(exactly = 1) { mockedPacketHelper.sendContainerClose(player) }
-//    }
-//
-//}
+    private lateinit var mockedPacketHelper: PacketHelper
+
+    @BeforeEach
+    fun setUp() {
+        mockedPacketHelper = mockk<PacketHelper>()
+        mockkObject(PacketHelper.Companion)
+        every { PacketHelper.instance } returns mockedPacketHelper
+        every { mockedPacketHelper.generateNextContainerId(any()) } returns 114
+    }
+
+    @AfterEach
+    fun tearDown() {
+        unmockkObject(PacketHelper.Companion)
+    }
+
+    @Test
+    fun `open receptacle and send init packets to player`() {
+        val player = mockk<Player>(relaxed = true)
+        every { player.uniqueId } returns UUID.randomUUID()
+
+        val title = Component.text("Phantom Receptacle Test")
+        val layout = ViewLayout.Chest.GENERIC_9X3
+
+        every {
+            mockedPacketHelper.sendOpenScreen(
+                player, 114, layout.type, title
+            )
+        } just Runs
+        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
+        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, null) } just Runs
+
+        val receptacle = PhantomReceptacle(title, layout).apply {
+            hidePlayerInventory = true // jump over player inventory setup
+        }
+        receptacle.open(player)
+
+        verify(exactly = 1) { mockedPacketHelper.generateNextContainerId(player) }
+        verify(exactly = 1) {
+            mockedPacketHelper.sendOpenScreen(
+                player, 114, layout.type, title
+            )
+        }
+        verify(exactly = 1) { mockedPacketHelper.sendContainerSetContent(player, 114, any()) }
+        verify(exactly = 1) { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, null) }
+    }
+
+    @Test
+    fun `set up player inventory slots and open`() {
+        val player = mockk<Player>(relaxed = true)
+        val inventory = mockk<PlayerInventory>(relaxed = true)
+        val inventoryContents = arrayOfNulls<ItemStack?>(36)
+
+        val apple = mockk<ItemStack>()
+        val bread = mockk<ItemStack>()
+        inventoryContents[0] = apple // hotbar slot: 0, slot index: 30
+        inventoryContents[17] = bread // main inv slot:17, slot index: 11
+
+        val title = Component.text("Phantom Receptacle Test")
+        val layout = ViewLayout.FixedContainer.ANVIL
+        val receptacle = PhantomReceptacle(title, layout).apply {
+            hidePlayerInventory = false
+        }
+
+        every { player.uniqueId } returns UUID.randomUUID()
+        every { player.inventory } returns inventory
+        every { inventory.contents } returns inventoryContents
+
+        every { mockedPacketHelper.sendOpenScreen(player, 114, layout.type, any()) } just Runs
+        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
+        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, null) } just Runs
+
+        receptacle.open(player)
+
+        // Verify that the player inventory slots are set up correctly
+        verify {
+            mockedPacketHelper.sendContainerSetContent(
+                player,
+                114,
+                match<Array<ItemStack?>> { items -> items[30] == apple && items[11] == bread }
+            )
+        }
+    }
+
+    @Test
+    fun `retitle receptacle and send update to player`() {
+        val player = mockk<Player>(relaxed = true)
+        every { player.uniqueId } returns UUID.randomUUID()
+
+        val initialTitle = Component.text("Initial Title")
+        val newTitle = Component.text("New Title")
+        val layout = ViewLayout.Chest.GENERIC_9X3
+
+        every {
+            mockedPacketHelper.sendOpenScreen(
+                player, 114, layout.type, any()
+            )
+        } just Runs
+        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
+        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, null) } just Runs
+
+        val receptacle = PhantomReceptacle(initialTitle, layout).apply {
+            hidePlayerInventory = true // jump over player inventory setup
+        }
+        receptacle.open(player)
+
+        mockkObject(TaskUtil)
+        every { TaskUtil.sync(period = 0L, now = false, delay = 3L, any()) } answers {
+            receptacle.initializationPackets()
+            mockk()
+        }
+
+        // Retitle the receptacle
+        receptacle.title(newTitle, render = true)
+
+        // Verify that the retitle packet was sent
+        verify(exactly = 1) {
+            mockedPacketHelper.sendOpenScreen(
+                player, 114, layout.type, newTitle
+            )
+        }
+
+        unmockkObject(TaskUtil)
+    }
+
+    @Test
+    fun `send close packet on close with render true`() {
+        val player = mockk<Player>(relaxed = true)
+        every { player.uniqueId } returns UUID.randomUUID()
+
+        val title = Component.text("Phantom Receptacle Test")
+        val layout = ViewLayout.Chest.GENERIC_9X3
+
+        every { mockedPacketHelper.sendOpenScreen(player, 114, layout.type, any()) } just Runs
+        every { mockedPacketHelper.sendContainerSetContent(player, 114, any()) } just Runs
+        every { mockedPacketHelper.sendContainerSetSlot(player, 0, 45, null) } just Runs
+        every { mockedPacketHelper.sendContainerClose(player) } just Runs
+
+        val receptacle = PhantomReceptacle(title, layout).apply {
+            hidePlayerInventory = true // jump over player inventory setup
+        }
+        receptacle.open(player)
+
+        // Close the receptacle with render = true
+        receptacle.close(render = true)
+
+        // Verify that the close packet was sent
+        verify(exactly = 1) { mockedPacketHelper.sendContainerClose(player) }
+    }
+
+}
